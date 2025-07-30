d54ef8d4446a32e3a1df69df7e696095528af5da
lstevensN <lstevens@student.neumont.edu>
2025-07-29 22:48:24 -0600

    Added Fish Lost Text & SFX

--- Assets/RedRiotAR/Scripts/FishCatchCelebrator.cs
+++ Assets/RedRiotAR/Scripts/FishCatchCelebrator.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class FishCatchCelebrator : MonoBehaviour
 {
-    [SerializeField] private Canvas TitleUI;
+    [SerializeField] private TMP_Text CelebrationText;
+    [SerializeField] private TMP_Text DespairText;
+
     [SerializeField] private AudioSource fishCaught;
+    [SerializeField] private AudioSource fishLost;
 
     private MashReel mashReel;
     private GameObject shownFish;
@@ -13,14 +17,17 @@ public class FishCatchCelebrator : MonoBehaviour
     {
         // This may not be the best way to do this, but it'll work for now!
         mashReel = FishManager.instance.mashReel;
-        mashReel.OnFishCaught += Celebrate;
 
-        if (TitleUI != null) TitleUI.enabled = false;
+        mashReel.OnFishCaught += Celebrate;
+        mashReel.OnFishLost += Despair;
+
+        if (CelebrationText != null) CelebrationText.enabled = false;
+        if (DespairText != null) DespairText.enabled = false;
     }
 
     private void Celebrate()
     {
-        if (TitleUI != null) TitleUI.enabled = true;  // Show celebratory text
+        if (CelebrationText != null) CelebrationText.enabled = true;  // Show celebratory text
         fishCaught.Play();
 
         // Uncomment if currentFishData is made public
@@ -35,20 +42,39 @@ public class FishCatchCelebrator : MonoBehaviour
 
             shownFish.transform.position = 7 * Camera.main.transform.forward + Camera.main.transform.position;  // Place fish in front of camera
 
-            shownFish.GetComponent<Animator>().enabled = false;  // Disable swimming animation
-            shownFish.GetComponent<FishBehavior>().caught = true;  // Turn off normal fish behavior
+            try
+            {
+                shownFish.GetComponent<Animator>().enabled = false;  // Disable swimming animation
+                shownFish.GetComponent<FishBehavior>().caught = true;  // Turn off normal fish behavior
+                shownFish.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, Mathf.Deg2Rad * 420, 0);
+            }
+            catch { }
 
-            shownFish.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, Mathf.Deg2Rad * 420, 0);
         }
 
-        StartCoroutine(Stop());
+        StartCoroutine(StopCelebrating());
     }
 
-    private IEnumerator Stop()
+    private void Despair()
+    {
+        if (DespairText != null) DespairText.enabled = true;  // Show despairing text
+        fishLost.Play();
+
+        StartCoroutine(StopDespairing());
+    }
+
+    private IEnumerator StopCelebrating()
     {
         yield return new WaitForSeconds(4f);  // Wait 5 seconds
 
-        if (TitleUI != null) TitleUI.enabled = false;  // Hide text
+        if (CelebrationText != null) CelebrationText.enabled = false;  // Hide text
         if (shownFish != null) Destroy(shownFish);  // Destroy fish
+    }
+
+    private IEnumerator StopDespairing()
+    {
+        yield return new WaitForSeconds(4f);  // Wait 5 seconds
+
+        if (DespairText != null) DespairText.enabled = false;  // Hide text
     }
 }
